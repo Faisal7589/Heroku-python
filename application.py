0b17bbe1c5a3adb8b3b7341292b301a99a536297
Faisal <faisal-theone1@outlook.com>
2022-04-09 05:25:02 +0400

    urmom

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
+
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -39,9 +40,9 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
+
 db = SQL("sqlite:///finance.db")
-
-
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 @app.route("/")
